ce2d0135a0143791191d1168dc02c02d2760013b
Arbaaz Khan <arbaaz9416@gmail.com>
2025-04-19 22:49:46 +0530

    fa

--- CampusAlumni/CampusAlumni/settings.py
+++ CampusAlumni/CampusAlumni/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-szl)977a0r818=xg56%dh4%tw3z(_nv5$8o1)xgab6hckkm%3c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["campusAlumni.onrender.com"]
+ALLOWED_HOSTS = ["campusalumnifiles.onrender.com"]
 
 
 # Application definition
